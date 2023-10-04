60b73d96a54f511745fb85ba70eb4042e00890c4
Abusantos <abusomwansantos@gmail.com>
2023-10-04 01:29:18 +0100

    updates

---
+++ billsManager/forms.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models  import User
+from django import forms
+
+
+# class SignUpForm(UserCreationForm):
--- billsManager/views.py
+++ billsManager/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
+
+
+
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #authenticate
+        user= authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You've been logged in!")
+            return redirect ('home')
+        else:
+            messages.success(request, "There was an error loggin in, please try again...")
+            return redirect ('home')
+    else:
+        return render(request, 'home.html',{})
+
+
+# def login_user(request):
+#     pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You've been logged out!")
+    return redirect ('home')
+
+def register(request):
+    return render(request, 'register.html',{})
+
